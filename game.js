c75033f7675f3e822faa68b52940d4b188ac75d3
Olga Thompson <olgatj2007@gmail.com>
2020-10-08 13:45:27 -0400

    added game.js

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+    constructor(name1, name2) {
+        this.name1 = name1;
+        this.name2 = name2;
+    }
+    getName() {
+        return `${this.name1} vs ${this.name2}`;
+    }
+}
+export default Game; 
