11a190157c2a2d85ed60c23e58cfcaf8a2beaa10
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-10-08 14:05:48 -0400

    Added event listeners for player1 & player2

--- connect-four.js
+++ connect-four.js
@@ -1,5 +1,27 @@
 import Game from './game.js';
 const game = undefined;
 document.addEventListener('DOMContentLoaded', (event) => {
-    
+  const player1 = document.getElementById('player-1-name');
+  const player2 = document.getElementById('player-2-name');
+  let newGame = document.getElementById('new-game')
+
+  if (!player1.value && !player2.value) {
+    newGame.disabled = true;
+  }
+
+  player1.addEventListener('keyup', event => {
+    if (player1.value && player2.value) {
+      newGame.disabled = false;
+    }
+  })
+
+  player2.addEventListener('keyup', event => {
+    if (player1.value && player2.value) {
+      newGame.disabled = false;
+    }
+  })
+
+  newGame.addEventListener('click', event => {
+
+  })
 })
