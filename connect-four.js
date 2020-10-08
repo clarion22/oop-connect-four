5bbd4735faf3f7dc91d01df4899f6b64ad06710b
Olga Thompson <olgatj2007@gmail.com>
2020-10-08 15:58:36 -0400

    added event listener for new game

--- connect-four.js
+++ connect-four.js
@@ -1,8 +1,22 @@
 import Game from './game.js';
-const game = undefined;
+const player1 = document.getElementById('player-1-name');
+const player2 = document.getElementById('player-2-name');
+let game = undefined;
+const board = document.getElementById('board-holder');
+const clickTarget = document.getElementById('click-targets');
+
+function updateUI() {
+    if (!game) {
+        board.classList.add('is-invisible');
+    }
+    board.classList.remove('is-invisible');
+    document
+        .getElementById('game-name')
+        .innerHTML = game.getName();
+    
+}
 document.addEventListener('DOMContentLoaded', (event) => {
-  const player1 = document.getElementById('player-1-name');
-  const player2 = document.getElementById('player-2-name');
+
   let newGame = document.getElementById('new-game')
 
   if (!player1.value && !player2.value) {
@@ -22,6 +36,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
   })
 
   newGame.addEventListener('click', event => {
-
+    game = new Game(player1.value, player2.value);
+    player1.value = '';
+    player2.value = '';
+    newGame.disabled = true;
+    updateUI();
+  })
+  clickTarget.addEventListener('click', event => {
+      game.playInColumn();
+      updateUI();
   })
 })
--- game.js
+++ game.js
@@ -2,9 +2,18 @@ class Game {
     constructor(name1, name2) {
         this.name1 = name1;
         this.name2 = name2;
+        this.currentPlayer = 1;
     }
     getName() {
         return `${this.name1} vs ${this.name2}`;
     }
+
+    playInColumn() {
+        if(this.currentPlayer % 2) {
+            this.currentPlayer = 1;
+        } else {
+            this.currentPlayer = 2; 
+        }
+    }
 }
-export default Game; 
+export default Game;
