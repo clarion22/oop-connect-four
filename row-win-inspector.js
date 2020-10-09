1fed12aa27600d2b0708dba549c024e2fe153afc
Olga Thompson <olgatj2007@gmail.com>
2020-10-09 18:56:55 -0400

    checkRow bug fix

--- row-win-inspector.js
+++ row-win-inspector.js
@@ -5,16 +5,16 @@ export default class RowWinInspector{
 
   inspect() {
     for (let j = 0; j < 6; j++) {
-      let column1 = this.columns[0].getTokenAt[j]
-      let column2 = this.columns[1].getTokenAt[j]
-      let column3 = this.columns[2].getTokenAt[j]
-      let column4 = this.columns[3].getTokenAt[j]
+      const column1 = this.columns[0].getTokenAt(j);
+      const column2 = this.columns[1].getTokenAt(j);
+      const column3 = this.columns[2].getTokenAt(j);
+      const column4 = this.columns[3].getTokenAt(j);
         if (column1 === column2 &&
           column2 === column3 &&
-          column3 === column4
-          // column1.getTokenAt[j] !== null
+          column3 === column4 &&
+          column1 !== null
         ) {
-          return column1.getTokenAt[j];
+          return column1;
         }
       }
       return 0;
